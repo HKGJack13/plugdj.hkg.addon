5d38efb336ac185d18a99162c08ed40f461aa53b
Jack13 <Jack13.hkgolden@gmail.com>
2014-07-01 22:18:06 +0800

    Add auto update ability.

--- goldenmusic.user.js
+++ goldenmusic.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name       PlugHKG IconPack 
-// @version  1.5.0
-// @description  Usage: Go To School By Bus
+// @version  1.5.1
+// @description  Provide hkgolden icons for plugdj users
+// @downloadURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.user.js
+// @downloadURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.meta.js
 // @match     http://plug.dj/hkgolden/*
 // @copyright   nasaorc , bus, jack13
 // ==/UserScript==
