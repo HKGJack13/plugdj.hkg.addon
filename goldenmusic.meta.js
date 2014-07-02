59491cc1d40bb904b00770fe873e7da1d6682342
Jack13 <Jack13.hkgolden@gmail.com>
2014-07-02 12:47:04 +0800

    find the problem, use updateURL instead of two downloadURL

--- goldenmusic.meta.js
+++ goldenmusic.meta.js
@@ -3,7 +3,7 @@
 // @version  1.5.1.1
 // @description  Provide hkgolden icons for plugdj users
 // @downloadURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.user.js
-// @downloadURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.meta.js
+// @updateURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.meta.js
 // @match     http://plug.dj/hkgolden/*
 // @copyright   nasaorc , bus, jack13
 // ==/UserScript==
--- goldenmusic.user.js
+++ goldenmusic.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name       PlugHKG IconPack 
-// @version  1.5.1.1
+// @version  1.5.1.2
 // @description  Provide hkgolden icons for plugdj users
 // @downloadURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.user.js
-// @downloadURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.meta.js
+// @updateURL https://raw.githubusercontent.com/HKGJack13/plugdj.hkg.addon/master/goldenmusic.meta.js
 // @match     http://plug.dj/hkgolden/*
 // @copyright   nasaorc , bus, jack13
 // ==/UserScript==
